bf96bbd28023e75bb0de9accfd4ea481d4a4b4b4
Jay Shenk <jay.shenk@gmail.com>
2015-12-30 21:49:15 -0500

    Solve Basics Ex. 3

---
+++ the_basics/3.rb
@@ -0,0 +1,13 @@
+# Write a program that uses a hash to store a list of movie titles
+# with the year they came out. Then use the puts command to make
+# your program print out the year of each movie to the screen.
+
+movies = { "Star Wars" => 1977, 
+           "The Matrix" => 1999, 
+           "The Fountain" => 2006, 
+           "Limitless" => 2011 }
+
+puts movies["Star Wars"]
+puts movies["The Matrix"]
+puts movies["The Fountain"]
+puts movies["Limitless"]
