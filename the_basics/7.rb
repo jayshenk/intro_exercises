29c65c77d3731181199ff07ce71ba3df288771ee
Jay Shenk <jay.shenk@gmail.com>
2016-01-02 16:32:10 -0500

    Solve Basics Ex. 7

---
+++ the_basics/7.rb
@@ -0,0 +1,6 @@
+# What does the following error message tell you?
+# SyntaxError: (irb):2: syntax error, unexpected ')', expecting '}'
+# from /usr/local/rvm/rubies/ruby-2.0.0-rc2/bin/irb:16:in `<main>'
+
+# That somewhere in your program there is an opening bracket without a corresponding closing bracket. 
+# This could have happened when creating a hash or a code block. 
