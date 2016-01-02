ee7b64e82e7f6f0cc84fe3885deb24f080681ad6
Jay Shenk <jay.shenk@gmail.com>
2016-01-02 17:16:46 -0500

    Solve Variables Ex. 4

--- variables/name.rb
+++ variables/name.rb
@@ -1,6 +1,11 @@
-puts "Please enter your name:"
-name = gets.chomp
+puts "What is your first name?"
+first_name = gets.chomp
 
-puts "Nice to meet you, #{name}!"
+puts "What is your last name?"
+last_name = gets.chomp
 
-10.times { puts name }
+full_name = "#{first_name} #{last_name}"
+
+puts "Nice to meet you, #{full_name}!"
+
+10.times { puts full_name }
