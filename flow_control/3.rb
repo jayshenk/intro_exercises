d68c43660d7567b93ff3c27574afe5813010a73a
Jay Shenk <jay.shenk@gmail.com>
2016-01-03 15:42:34 -0500

    Refactor Flow Control Ex. 3

--- flow_control/3.rb
+++ flow_control/3.rb
@@ -3,10 +3,10 @@ number = gets.chomp.to_i
 
 if number < 0
   puts "You can't enter a negative number!"
-if number >= 0 and number <= 50
+elsif number <= 50
   puts "#{number} is between 0 and 50."
-elsif number >= 51 and number <= 100
+elsif number <= 100
   puts "#{number} is between 51 and 100."
-elsif number > 100
+else
   puts "#{number} is greater than 100."
 end    
