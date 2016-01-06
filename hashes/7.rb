4adeb14f00b09bb0aee0456cbcf7a5009a332a41
Jay Shenk <jay.shenk@gmail.com>
2016-01-05 21:02:29 -0500

    Solve Hashes Ex. 7

---
+++ hashes/7.rb
@@ -0,0 +1,6 @@
+x = "hi there"
+my_hash = {x: "some value"}
+my_hash2 = {x => "some value"}
+
+# The first hash uses a new symbol x as the key while the second hash uses the string "hi there"
+# as the key.
